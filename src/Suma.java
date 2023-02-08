323dfbada656f274596e7a9e0b4f9ac2f2ef3e09
Mario vg <22670080@itiguala.edu.mx>
2023-02-08 07:49:55 -0600

    Practica 1 terminada - Mario Hasinm Zarek Vega Rosendo

---
+++ src/Suma.java
@@ -0,0 +1,13 @@
+public class Suma {
+    public static void main(String[] args) {
+        int n1=2;
+        int n2=2;
+        int r;
+
+        r=n1+n2;
+        System.out.println("La suma de es: "+r);
+
+    }
+}
+
+
